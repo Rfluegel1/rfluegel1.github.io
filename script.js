096c599f2bce461d7bf27a1cf6cd0770b935a363
Rfluegel1 <reid.fluegel@yahoo.com>
2019-10-28 09:46:20 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,123 @@
+
+var wordArray = ["a" ,"ability" ,"able" ,"about" ,"above" ,"accept" ,"according" ,"account" ,"across" ,"act" ,"action" ,"activity" ,"actually" ,"add" ,"address" ,"administration" ,"admit" ,"adult" ,"affect" ,"after" ,"again" ,"against" ,"age" ,"agency" ,"agent" ,"ago" ,"agree" ,"agreement" ,"ahead" ,"air" ,"all" ,"allow" ,"almost" ,"alone" ,"along" ,"already" ,"also" ,"although" ,"always" ,"American" ,"among" ,"amount" ,"analysis" ,"and" ,"animal" ,"another" ,"answer" ,"any" ,"anyone" ,"anything" ,"appear" ,"apply" ,"approach" ,"area" ,"argue" ,"arm" ,"around" ,"arrive" ,"art" ,"article" ,"artist" ,"as" ,"ask" ,"assume" ,"at" ,"attack" ,"attention" ,"attorney" ,"audience" ,"author" ,"authority" ,"available" ,"avoid" ,"away" ,"baby" ,"back" ,"bad" ,"bag" ,"ball" ,"bank" ,"bar" ,"base" ,"be" ,"beat" ,"beautiful" ,"because" ,"become" ,"bed" ,"before" ,"begin" ,"behavior" ,"behind" ,"believe" ,"benefit" ,"best" ,"better" ,"between" ,"beyond" ,"big" ,"bill" ,"billion" ,"bit" ,"black" ,"blood" ,"blue" ,"board" ,"body" ,"book" ,"born" ,"both" ,"box" ,"boy" ,"break" ,"bring" ,"brother" ,"budget" ,"build" ,"building" ,"business" ,"but" ,"buy" ,"by" ,"call" ,"camera" ,"campaign" ,"can" ,"cancer" ,"candidate" ,"capital" ,"car" ,"card" ,"care" ,"career" ,"carry" ,"case" ,"catch" ,"cause" ,"cell" ,"center" ,"central" ,"century" ,"certain" ,"certainly" ,"chair" ,"challenge" ,"chance" ,"change" ,"character" ,"charge" ,"check" ,"child" ,"choice" ,"choose" ,"church" ,"citizen" ,"city" ,"civil" ,"claim" ,"class" ,"clear" ,"clearly" ,"close" ,"coach" ,"cold" ,"collection" ,"college" ,"color" ,"come" ,"commercial" ,"common" ,"community" ,"company" ,"compare" ,"computer" ,"concern" ,"condition" ,"conference" ,"Congress" ,"consider" ,"consumer" ,"contain" ,"continue" ,"control" ,"cost" ,"could" ,"country" ,"couple" ,"course" ,"court" ,"cover" ,"create" ,"crime" ,"cultural" ,"culture" ,"cup" ,"current" ,"customer" ,"cut" ,"dark" ,"data" ,"daughter" ,"day" ,"dead" ,"deal" ,"death" ,"debate" ,"decade" ,"decide" ,"decision" ,"deep" ,"defense" ,"degree" ,"Democrat" ,"democratic" ,"describe" ,"design" ,"despite" ,"detail" ,"determine" ,"develop" ,"development" ,"die" ,"difference" ,"different" ,"difficult" ,"dinner" ,"direction" ,"director" ,"discover" ,"discuss" ,"discussion" ,"disease" ,"do" ,"doctor" ,"dog" ,"door" ,"down" ,"draw" ,"dream" ,"drive" ,"drop" ,"drug" ,"during" ,"each" ,"early" ,"east" ,"easy" ,"eat" ,"economic" ,"economy" ,"edge" ,"education" ,"effect" ,"effort" ,"eight" ,"either" ,"election" ,"else" ,"employee" ,"end" ,"energy" ,"enjoy" ,"enough" ,"enter" ,"entire" ,"environment" ,"environmental" ,"especially" ,"establish" ,"even" ,"evening" ,"event" ,"ever" ,"every" ,"everybody" ,"everyone" ,"everything" ,"evidence" ,"exactly" ,"example" ,"executive" ,"exist" ,"expect" ,"experience" ,"expert" ,"explain" ,"eye" ,"face" ,"fact" ,"factor" ,"fail" ,"fall" ,"family" ,"far" ,"fast" ,"father" ,"fear" ,"federal" ,"feel" ,"feeling" ,"few" ,"field" ,"fight" ,"figure" ,"fill" ,"film" ,"final" ,"finally" ,"financial" ,"find" ,"fine" ,"finger" ,"finish" ,"fire" ,"firm" ,"first" ,"fish" ,"five" ,"floor" ,"fly" ,"focus" ,"follow" ,"food" ,"foot" ,"for" ,"force" ,"foreign" ,"forget" ,"form" ,"former" ,"forward" ,"four" ,"free" ,"friend" ,"from" ,"front" ,"full" ,"fund" ,"future" ,"game" ,"garden" ,"gas" ,"general" ,"generation" ,"get" ,"girl" ,"give" ,"glass" ,"go" ,"goal" ,"good" ,"government" ,"great" ,"green" ,"ground" ,"group" ,"grow" ,"growth" ,"guess" ,"gun" ,"guy" ,"hair" ,"half" ,"hand" ,"hang" ,"happen" ,"happy" ,"hard" ,"have" ,"he" ,"head" ,"health" ,"hear" ,"heart" ,"heat" ,"heavy" ,"help" ,"her" ,"here" ,"herself" ,"high" ,"him" ,"himself" ,"his" ,"history" ,"hit" ,"hold" ,"home" ,"hope" ,"hospital" ,"hot" ,"hotel" ,"hour" ,"house" ,"how" ,"however" ,"huge" ,"human" ,"hundred" ,"husband" ,"I" ,"idea" ,"identify" ,"if" ,"image" ,"imagine" ,"impact" ,"important" ,"improve" ,"in" ,"include" ,"including" ,"increase" ,"indeed" ,"indicate" ,"individual" ,"industry" ,"information" ,"inside" ,"instead" ,"institution" ,"interest" ,"interesting" ,"international" ,"interview" ,"into" ,"investment" ,"involve" ,"issue" ,"it" ,"item" ,"its" ,"itself" ,"job" ,"join" ,"just" ,"keep" ,"key" ,"kid" ,"kill" ,"kind" ,"kitchen" ,"know" ,"knowledge" ,"land" ,"language" ,"large" ,"last" ,"late" ,"later" ,"laugh" ,"law" ,"lawyer" ,"lay" ,"lead" ,"leader" ,"learn" ,"least" ,"leave" ,"left" ,"leg" ,"legal" ,"less" ,"let" ,"letter" ,"level" ,"lie" ,"life" ,"light" ,"like" ,"likely" ,"line" ,"list" ,"listen" ,"little" ,"live" ,"local" ,"long" ,"look" ,"lose" ,"loss" ,"lot" ,"love" ,"low" ,"machine" ,"magazine" ,"main" ,"maintain" ,"major" ,"majority" ,"make" ,"man" ,"manage" ,"management" ,"manager" ,"many" ,"market" ,"marriage" ,"material" ,"matter" ,"may" ,"maybe" ,"me" ,"mean" ,"measure" ,"media" ,"medical" ,"meet" ,"meeting" ,"member" ,"memory" ,"mention" ,"message" ,"method" ,"middle" ,"might" ,"military" ,"million" ,"mind" ,"minute" ,"miss" ,"mission" ,"model" ,"modern" ,"moment" ,"money" ,"month" ,"more" ,"morning" ,"most" ,"mother" ,"mouth" ,"move" ,"movement" ,"movie" ,"Mr" ,"Mrs" ,"much" ,"music" ,"must" ,"my" ,"myself" ,"name" ,"nation" ,"national" ,"natural" ,"nature" ,"near" ,"nearly" ,"necessary" ,"need" ,"network" ,"never" ,"new" ,"news" ,"newspaper" ,"next" ,"nice" ,"night" ,"no" ,"none" ,"nor" ,"north" ,"not" ,"note" ,"nothing" ,"notice" ,"now" ,"n't" ,"number" ,"occur" ,"of" ,"off" ,"offer" ,"office" ,"officer" ,"official" ,"often" ,"oh" ,"oil" ,"ok" ,"old" ,"on" ,"once" ,"one" ,"only" ,"onto" ,"open" ,"operation" ,"opportunity" ,"option" ,"or" ,"order" ,"organization" ,"other" ,"others" ,"our" ,"out" ,"outside" ,"over" ,"own" ,"owner" ,"page" ,"pain" ,"painting" ,"paper" ,"parent" ,"part" ,"participant" ,"particular" ,"particularly" ,"partner" ,"party" ,"pass" ,"past" ,"patient" ,"pattern" ,"pay" ,"peace" ,"people" ,"per" ,"perform" ,"performance" ,"perhaps" ,"period" ,"person" ,"personal" ,"phone" ,"physical" ,"pick" ,"picture" ,"piece" ,"place" ,"plan" ,"plant" ,"play" ,"player" ,"PM" ,"point" ,"police" ,"policy" ,"political" ,"politics" ,"poor" ,"popular" ,"population" ,"position" ,"positive" ,"possible" ,"power" ,"practice" ,"prepare" ,"present" ,"president" ,"pressure" ,"pretty" ,"prevent" ,"price" ,"private" ,"probably" ,"problem" ,"process" ,"produce" ,"product" ,"production" ,"professional" ,"professor" ,"program" ,"project" ,"property" ,"protect" ,"prove" ,"provide" ,"public" ,"pull" ,"purpose" ,"push" ,"put" ,"quality" ,"question" ,"quickly" ,"quite" ,"race" ,"radio" ,"raise" ,"range" ,"rate" ,"rather" ,"reach" ,"read" ,"ready" ,"real" ,"reality" ,"realize" ,"really" ,"reason" ,"receive" ,"recent" ,"recently" ,"recognize" ,"record" ,"red" ,"reduce" ,"reflect" ,"region" ,"relate" ,"relationship" ,"religious" ,"remain" ,"remember" ,"remove" ,"report" ,"represent" ,"Republican" ,"require" ,"research" ,"resource" ,"respond" ,"response" ,"responsibility" ,"rest" ,"result" ,"return" ,"reveal" ,"rich" ,"right" ,"rise" ,"risk" ,"road" ,"rock" ,"role" ,"room" ,"rule" ,"run" ,"safe" ,"same" ,"save" ,"say" ,"scene" ,"school" ,"science" ,"scientist" ,"score" ,"sea" ,"season" ,"seat" ,"second" ,"section" ,"security" ,"see" ,"seek" ,"seem" ,"sell" ,"send" ,"senior" ,"sense" ,"series" ,"serious" ,"serve" ,"service" ,"set" ,"seven" ,"several" ,"sex" ,"sexual" ,"shake" ,"share" ,"she" ,"shoot" ,"short" ,"shot" ,"should" ,"shoulder" ,"show" ,"side" ,"sign" ,"significant" ,"similar" ,"simple" ,"simply" ,"since" ,"sing" ,"single" ,"sister" ,"sit" ,"site" ,"situation" ,"six" ,"size" ,"skill" ,"skin" ,"small" ,"smile" ,"so" ,"social" ,"society" ,"soldier" ,"some" ,"somebody" ,"someone" ,"something" ,"sometimes" ,"son" ,"song" ,"soon" ,"sort" ,"sound" ,"source" ,"south" ,"southern" ,"space" ,"speak" ,"special" ,"specific" ,"speech" ,"spend" ,"sport" ,"spring" ,"staff" ,"stage" ,"stand" ,"standard" ,"star" ,"start" ,"state" ,"statement" ,"station" ,"stay" ,"step" ,"still" ,"stock" ,"stop" ,"store" ,"story" ,"strategy" ,"street" ,"strong" ,"structure" ,"student" ,"study" ,"stuff" ,"style" ,"subject" ,"success" ,"successful" ,"such" ,"suddenly" ,"suffer" ,"suggest" ,"summer" ,"support" ,"sure" ,"surface" ,"system" ,"table" ,"take" ,"talk" ,"task" ,"tax" ,"teach" ,"teacher" ,"team" ,"technology" ,"television" ,"tell" ,"ten" ,"tend" ,"term" ,"test" ,"than" ,"thank" ,"that" ,"the" ,"their" ,"them" ,"themselves" ,"then" ,"theory" ,"there" ,"these" ,"they" ,"thing" ,"think" ,"third" ,"this" ,"those" ,"though" ,"thought" ,"thousand" ,"threat" ,"three" ,"through" ,"throughout" ,"throw" ,"thus" ,"time" ,"to" ,"today" ,"together" ,"tonight" ,"too" ,"top" ,"total" ,"tough" ,"toward" ,"town" ,"trade" ,"traditional" ,"training" ,"travel" ,"treat" ,"treatment" ,"tree" ,"trial" ,"trip" ,"trouble" ,"true" ,"truth" ,"try" ,"turn" ,"TV" ,"two" ,"type" ,"under" ,"understand" ,"unit" ,"until" ,"up" ,"upon" ,"us" ,"use" ,"usually" ,"value" ,"various" ,"very" ,"victim" ,"view" ,"violence" ,"visit" ,"voice" ,"vote" ,"wait" ,"walk" ,"wall" ,"want" ,"war" ,"watch" ,"water" ,"way" ,"we" ,"weapon" ,"wear" ,"week" ,"weight" ,"well" ,"west" ,"western" ,"what" ,"whatever" ,"when" ,"where" ,"whether" ,"which" ,"while" ,"white" ,"who" ,"whole" ,"whom" ,"whose" ,"why" ,"wide" ,"wife" ,"will" ,"win" ,"wind" ,"window" ,"wish" ,"with" ,"within" ,"without" ,"woman" ,"wonder" ,"word" ,"work" ,"worker" ,"world" ,"worry" ,"would" ,"write" ,"writer" ,"wrong" ,"yard" ,"yeah" ,"year" ,"yes" ,"yet" ,"you" ,"young" ,"your" ,"yourself"];
+
+var leftHandLetters = ["q", "a", "z", "w", "s", "x", "e", "d", "c", "r", "f", "v", "t", "g", "b"];
+var rightHandLetters = ["y", "h", "n", "u", "j", "m", "i", "k", "o", "l", "p"];
+
+var max = document.getElementById("maxWordLength").value;
+var min = document.getElementById("minWordLength").value;
+
+setMaxWordLength = function() {
+  max = document.getElementById("maxWordLength").value;
+}
+
+setMinWordLength = function() {
+  min = document.getElementById("minWordLength").value;
+}
+
+replaceAt = function(word, index, replacement) {
+  return word.substr(0, index) + replacement + word.substr(index + replacement.length);
+}
+
+leetTranslate = function(pwString, i) {
+    switch(pwString.charAt(i)){
+      case "a":
+        return replaceAt(pwString, i, "4");
+      case("b"):
+        return replaceAt(pwString, i, "8");
+      case("e"):
+        return replaceAt(pwString, i, "3");
+      case("g"):
+        return replaceAt(pwString, i, "6");
+      case("l"):
+        return replaceAt(pwString, i, "1");
+      case("o"):
+        return replaceAt(pwString, i, "0");
+      case("q"):
+        return replaceAt(pwString, i, "9");
+      case("s"):
+        return replaceAt(pwString, i, "5");
+      case("t"):
+        return replaceAt(pwString, i, "7");
+      case("z"):
+        return replaceAt(pwString, i, "2");
+      default:
+        return pwString;
+    }
+}
+
+easyTyping = function(word) {
+  for(var i = 0; i < word.length; i++){
+    if(i%2==0){
+      if(leftHandLetters.includes(word.charAt(i))==false){
+        console.log("false " + word);
+        return false;
+      }
+    }else{
+      if(rightHandLetters.includes(word.charAt(i))==false){
+        console.log("false " + word);
+        return false;
+      }
+    }
+  }
+  console.log("true " + word);
+  return true;
+}
+
+getPassword = function(){
+  var word = "";
+  while(!(word.length>=min && word.length<=max)){
+    word = wordArray[Math.floor(Math.random()*1000)]
+    if(document.getElementById("easyTyping").checked==true){
+      if(easyTyping(word)==false){
+        word = "";
+      }
+    }
+  }
+  if (document.getElementById("numSub").checked==true){
+    for(var i = 0; i < word.length; i++){
+      word = leetTranslate(word, i);
+    }
+  }
+  return word;
+}
+
+test = function() {
+  table = document.getElementById("pwTable");
+  tr = document.createElement("tr");
+  td1 = document.createElement("td");
+  td2 = document.createElement("td");
+  td3 = document.createElement("td");
+  td4 = document.createElement("td");
+  td5 = document.createElement("td");
+  var word1 = getPassword();
+  var word2 = getPassword();
+  var word3 = getPassword();
+  var word4 = getPassword();
+  console.log(word1.length+word2.length+word3.length+word4.length);
+  console.log(document.getElementById("maxPasswordLength").value);
+  var maxPasswordLength = document.getElementById("maxPasswordLength").value;
+  while(word1.length+word2.length+word3.length+word4.length > maxPasswordLength){
+    console.log("too long")
+    word1 = getPassword();
+    word2 = getPassword();
+    word3 = getPassword();
+    word4 = getPassword();
+  }
+  contents1 = document.createTextNode(word1);
+  contents2 = document.createTextNode(word2);
+  contents3 = document.createTextNode(word3);
+  contents4 = document.createTextNode(word4);
+  contents5 = document.createTextNode(maxPasswordLength);
+  td1.appendChild(contents1);
+  td2.appendChild(contents2);
+  td3.appendChild(contents3);
+  td4.appendChild(contents4);
+  td5.appendChild(contents5);
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+  tr.appendChild(td4);
+  tr.appendChild(td5);
+  table.appendChild(tr);
+}
